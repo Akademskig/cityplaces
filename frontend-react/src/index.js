ecbedf62fb8eb3cffe83d5161e2d30c716d5fa71
marta <Akademskig@gmail.com>
2019-01-09 19:45:14 +0100

    Add error handler

--- frontend-react/src/index.js
+++ frontend-react/src/index.js
@@ -2,15 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 import "semantic-ui-css/semantic.min.css"
-
+import 'react-notifications/lib/notifications.css';
 import './css/index.css';
 import * as serviceWorker from './serviceWorker';
 import MainRoutes from './routes';
+import ErrorHandler from './components/ErrorHandler';
 
 ReactDOM.render(
-    <BrowserRouter>
-        <MainRoutes />
-    </BrowserRouter>, document.getElementById('root'));
+    <ErrorHandler>
+        <BrowserRouter>
+            <MainRoutes />
+        </BrowserRouter>
+    </ErrorHandler>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
