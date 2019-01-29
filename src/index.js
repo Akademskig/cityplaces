cedefefadf89668f81ced81c3ccd939b7815cd91
marta <Akademskig@gmail.com>
2019-01-29 19:13:10 +0100

    catch errors in init function

--- src/index.js
+++ src/index.js
@@ -4,25 +4,22 @@ import { ColorConsole } from './utils/color-console'
 import { WebServer } from './web-server'
 import { Db } from './db/mongodb'
 import config from './config/config'
-import GoogleApi from './controllers/google-api.controller'
+
+
 
 const log = new ColorConsole()
 
-async function init(callback) {
+async function init() {
 	const webServer = new WebServer(log, config)
 	const mongoDb = new Db(log, config)
+
 	await webServer.init()
 	await mongoDb.init()
-
-	callback()
 }
 
-try {
-	init(() => {
-		log.blue('App started.')
-	})
-} catch (err) {
-	log.red(err)
-}
+init()
+	.then(() => log.blue('App started.'))
+	.catch(err => log.red(err))
+
 
 
