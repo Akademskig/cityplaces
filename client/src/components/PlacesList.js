b470014e2cf6d2c3e47f2b0a1aa5f14abef19826
Marta Sušek <marta.susek@uniqcast.com>
2019-05-21 20:34:39 +0200

    Render placesList by componentType prop

--- client/src/components/PlacesList.js
+++ client/src/components/PlacesList.js
@@ -45,7 +45,7 @@ export default class PlacesList extends Component {
     }
 
     render() {
-        if (this.props.currentPosition.lat)
+        if (this.props.currentPosition.lat ||  (this.props.componentType==="SAVED" || this.props.componentType==="OTHER") )
             return (
                 <div>
                     <div hidden={!this.state.mapVisible} className="map-container ">
@@ -79,7 +79,7 @@ export default class PlacesList extends Component {
                 </div >
             )
         else
-            return (<div></div>)
+            return (<p style={{textAlign:"center"}}>Unable to list places</p>)
     }
 }
 
